c1f6220a8a5ee9d65a7c13ecd059e47553520339
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-04-22 22:31:25 +0900

    メインページを別ファイルに移動

---
+++ lib/random_words.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item) {
+        // 偶数行なら区切り線を作成、 奇数行なら、_buildRowメソッドを実行
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if(index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('タイトル'),
+        centerTitle: true,
+      ),
+      body: _buildList(),
+    );
+  }
+}
