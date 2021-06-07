4ddcbc7482d2265bb3c45424af2a1459cafba3d0
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-06-08 05:53:29 +0900

    ボタン作成

--- lib/page/navigator_page.dart
+++ lib/page/navigator_page.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 class NavigatorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ページ遷移'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('画面遷移１'),
+              onPressed: () {
+                
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
