381d449a1c6766865afbb5e3a5f21c6e8d3fbe76
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-06-08 06:00:51 +0900

    次ページ作成

---
+++ lib/page/navigator_page/next_page1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NextPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('次ページ1'),
+      ),
+      body: Center(
+        child: Text('次ページ1'),
+      ),
+    );
+  }
+}
