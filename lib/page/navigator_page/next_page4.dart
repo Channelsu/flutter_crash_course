59f35735032caf68e23cf8f7ab0562b5fbe49c37
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-06-11 22:56:29 +0900

    次ページ4作成

---
+++ lib/page/navigator_page/next_page4.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NextPage4 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('次ページ4'),
+      ),
+      body: Center(
+        child: Text('次ページ4'),
+      ),
+    );
+  }
+}
