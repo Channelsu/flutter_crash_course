c614b756033d34b7b70469cefc60ae7ed7fd81cd
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-06-13 23:26:53 +0900

    デバイスの横幅と高さを表示

--- lib/page/media_query_page/media_query_page.dart
+++ lib/page/media_query_page/media_query_page.dart
@@ -4,9 +4,15 @@ class MediaQueryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('メディアクエリ'),
+      ),
+      body: Center(
+        child: Text('このデバイスは横$screenWidth 縦$screenHeightです。'),
       ),
     );
   }
