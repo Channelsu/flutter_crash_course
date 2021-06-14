7b306b7e30e03a90ebfb498b24e4d556ac0344e9
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-06-15 05:59:42 +0900

    画面の向き判定処理実装

--- lib/page/media_query_page/media_query_page.dart
+++ lib/page/media_query_page/media_query_page.dart
@@ -6,6 +6,7 @@ class MediaQueryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
     var screenHeight = MediaQuery.of(context).size.height;
+    var screenOrientation = MediaQuery.of(context).orientation;
 
     return Scaffold(
       appBar: AppBar(
@@ -18,7 +19,11 @@ class MediaQueryPage extends StatelessWidget {
             SizedBox(height: 20,),
             screenWidth >= 1200 && screenHeight >= 720
               ? Container(height: 100, width: 700, color: Colors.red,)
-              : Container(height: 100, width: 100, color: Colors.blue,)
+              : Container(height: 100, width: 100, color: Colors.blue,),
+            SizedBox(height: 20,),
+            screenOrientation == Orientation.portrait
+              ? Text('このデバイスは縦向きです。')
+              : Text('このデバイスは横向きです。')
           ],
         ),
       ),
