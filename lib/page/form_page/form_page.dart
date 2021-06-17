a227d8970807b739c607a33b528720837824d831
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-06-17 23:37:01 +0900

    チェックボックス追記

--- lib/page/form_page/form_page.dart
+++ lib/page/form_page/form_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class FormPage extends StatelessWidget {
+class FormPage extends StatefulWidget {
+  @override
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  bool value = false;
 
   @override
   Widget build(BuildContext context) {
@@ -8,13 +14,13 @@ class FormPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('フォーム'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('フォーム'),
-          ],
-        ),
+      body: ListView(
+        children: [
+          Checkbox(
+            value: value,
+            onChanged: (value) => setState(() => this.value = value),
+          ),
+        ],
       ),
     );
   }
