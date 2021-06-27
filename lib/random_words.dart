566bacdde81a87bf40709ce49734e218541a3a1b
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-06-27 22:38:12 +0900

    ドロワーに天気ページ追加

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:fluttercrashcourse/page/form_page/form_page.dart';
+import 'package:fluttercrashcourse/page/form_page/weather_page/weather_page.dart';
 import 'package:fluttercrashcourse/page/media_query_page/media_query_page.dart';
 import 'package:fluttercrashcourse/page/navigator_page/navigator_page.dart';
 
@@ -135,6 +136,17 @@ class _RandomWordsState extends State<RandomWords> {
                   context,
                   MaterialPageRoute(
                     builder: (context) => FormPage(),
+                  )
+                );
+              },
+            ),
+            ListTile(
+              title: Text('天気'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => WeatherPage(),
                   )
                 );
               },
