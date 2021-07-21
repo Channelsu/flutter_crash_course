442783ed47e6060100c4053aebf422f83e0da516
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-07-21 20:07:33 +0900

    appbar追記

--- lib/page/color_picker_page/color_picker_page.dart
+++ lib/page/color_picker_page/color_picker_page.dart
@@ -7,9 +7,10 @@ class ColorPickerPage extends StatefulWidget {
 
 class _ColorPickerPageState extends State<ColorPickerPage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('カラーピッカー'),
+      centerTitle: true,
+    ),
+  );
 }
