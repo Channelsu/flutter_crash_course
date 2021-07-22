a70ecdb87e6d88cfd986d86c76c204988b19d1d0
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-07-22 22:46:08 +0900

    中央にサークル配置

--- lib/page/color_picker_page/color_picker_page.dart
+++ lib/page/color_picker_page/color_picker_page.dart
@@ -6,11 +6,23 @@ class ColorPickerPage extends StatefulWidget {
 }
 
 class _ColorPickerPageState extends State<ColorPickerPage> {
+  Color color = Colors.red;
+  
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
       title: Text('カラーピッカー'),
       centerTitle: true,
+    ),
+    body: Center(
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
+        width: 120,
+        height: 120,
+      ),
     ),
   );
 }
