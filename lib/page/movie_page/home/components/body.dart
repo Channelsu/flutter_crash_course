6d7e262af5cdcca8312c2eb0e75d1dedc61c4c34
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-10 15:52:06 +0900

    カテゴリーリスト別ファイル化

--- lib/page/movie_page/home/components/body.dart
+++ lib/page/movie_page/home/components/body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluttercrashcourse/constants.dart';
+import 'package:fluttercrashcourse/page/movie_page/home/components/categories.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -8,63 +8,6 @@ class Body extends StatelessWidget {
       children: [
         CategoryList(),
       ],
-    );
-  }
-}
-
-// カテゴリーの状態を切り替えるので、statefull widgetでなければならない。
-class CategoryList extends StatefulWidget {
-  @override
-  _CategoryListState createState() => _CategoryListState();
-}
-
-class _CategoryListState extends State<CategoryList> {
-  int selectedCategory = 0;
-  List<String> categories = ['上映中', '人気', '近日公開'];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        itemBuilder: (BuildContext context, int index) => buildCategory(index, context),
-      ),
-    );
-  }
-
-  Padding buildCategory(int index, BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            selectedCategory = index;   
-          });
-        },
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              categories[index],
-              style: Theme.of(context).textTheme.headline6.copyWith(
-                fontWeight: FontWeight.w600,
-                color: index == selectedCategory ? kTextColor : Colors.black.withOpacity(0.4),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-              height: 6,
-              width: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: index == selectedCategory ? kSecondaryColor : Colors.transparent,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
