3b54f77733c3f6a9ba4d5fb766e064a6b5f146cf
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-16 06:45:32 +0900

    映画のカテゴリーの作成

--- lib/page/movie_page/home/components/body.dart
+++ lib/page/movie_page/home/components/body.dart
@@ -26,8 +26,34 @@ class Genres extends StatelessWidget {
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: genres.length,
-        itemBuilder: (BuildContext context, int index) => Text(
-          genres[index],
+        itemBuilder: (BuildContext context, int index) => GenreCard(genre: genres[index],),
+      ),
+    );
+  }
+}
+
+class GenreCard extends StatelessWidget {
+  final String genre;
+  const GenreCard({ Key key, this.genre }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(left: kDefaultPadding),
+      padding: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding / 4,  // 上下に5のpaddingを設定
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black26),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        genre,
+        style: TextStyle(
+          color: kTextColor.withOpacity(0.8),
+          fontSize: 16
         ),
       ),
     );
