78f16580b51e1b081c166bf32947f912f1f6e20d
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-16 06:49:40 +0900

    GenreCard別ファイル化

--- lib/page/movie_page/home/components/body.dart
+++ lib/page/movie_page/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttercrashcourse/components/genre_card.dart';
 import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/page/movie_page/home/components/categories.dart';
 
@@ -27,34 +28,6 @@ class Genres extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: genres.length,
         itemBuilder: (BuildContext context, int index) => GenreCard(genre: genres[index],),
-      ),
-    );
-  }
-}
-
-class GenreCard extends StatelessWidget {
-  final String genre;
-  const GenreCard({ Key key, this.genre }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      margin: EdgeInsets.only(left: kDefaultPadding),
-      padding: EdgeInsets.symmetric(
-        horizontal: kDefaultPadding,
-        vertical: kDefaultPadding / 4,  // 上下に5のpaddingを設定
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black26),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Text(
-        genre,
-        style: TextStyle(
-          color: kTextColor.withOpacity(0.8),
-          fontSize: 16
-        ),
       ),
     );
   }
