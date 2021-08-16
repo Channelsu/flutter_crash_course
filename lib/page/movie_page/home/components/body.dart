5ba748709a957d7f92c14cce15b2e0b9fa3da076
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-17 07:13:02 +0900

    MovieCardの映画評価部分の作成

--- lib/page/movie_page/home/components/body.dart
+++ lib/page/movie_page/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:fluttercrashcourse/components/genres.dart';
 import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
@@ -64,6 +65,7 @@ class MovieCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
       child: Column(
         children: [
+          // 映画画像部分
           Expanded(
             child: Container(
               decoration: BoxDecoration(
@@ -76,13 +78,29 @@ class MovieCard extends StatelessWidget {
               ),
             ),
           ),
+          // 映画タイトル部分
           Padding(
             padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
             child: Text(
               movie.title,
               style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.w600),
             ),
-          )
+          ),
+          // 映画評価部分
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/star_fill.svg',
+                height: 20,
+              ),
+              SizedBox(width: kDefaultPadding / 2,),
+              Text(
+                '${movie.rating}',
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ],
+          ),
         ],
       ),
     );
