84acb8b04cf253c5e22e5aabedc76b8785c480bd
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-18 06:06:09 +0900

    MovieCard別ファイル化

--- lib/page/movie_page/home/components/body.dart
+++ lib/page/movie_page/home/components/body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:fluttercrashcourse/components/genres.dart';
 import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
 import 'package:fluttercrashcourse/page/movie_page/home/components/categories.dart';
+import 'package:fluttercrashcourse/page/movie_page/home/components/movie_card.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -49,59 +49,6 @@ class _MovieCarouselState extends State<MovieCarousel> {
           itemCount: movies.length,
           itemBuilder: (context, index) => MovieCard(movie: movies[index],)
         ),
-      ),
-    );
-  }
-}
-
-class MovieCard extends StatelessWidget {
-  final Movie movie;
-  // コンストラクタを右クリックで作成
-  const MovieCard({Key key, this.movie}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Column(
-        children: [
-          // 映画画像部分
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                boxShadow: [kDefaultShadow],
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(movie.poster)
-                )
-              ),
-            ),
-          ),
-          // 映画タイトル部分
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-            child: Text(
-              movie.title,
-              style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          // 映画評価部分
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                'assets/icons/star_fill.svg',
-                height: 20,
-              ),
-              SizedBox(width: kDefaultPadding / 2,),
-              Text(
-                '${movie.rating}',
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
---
+++ lib/page/movie_page/home/components/movie_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttercrashcourse/constants.dart';
+import 'package:fluttercrashcourse/models/movie.dart';
+
+class MovieCard extends StatelessWidget {
+  final Movie movie;
+  // コンストラクタを右クリックで作成
+  const MovieCard({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Column(
+        children: [
+          // 映画画像部分
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [kDefaultShadow],
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(movie.poster)
+                )
+              ),
+            ),
+          ),
+          // 映画タイトル部分
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+            child: Text(
+              movie.title,
+              style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.w600),
+            ),
+          ),
+          // 映画評価部分
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/star_fill.svg',
+                height: 20,
+              ),
+              SizedBox(width: kDefaultPadding / 2,),
+              Text(
+                '${movie.rating}',
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
