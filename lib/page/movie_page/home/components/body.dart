978c93de89735d054154e130f7e368d51004198a
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-19 06:42:18 +0900

    buildMovieSliderウィジェット作成

--- lib/page/movie_page/home/components/body.dart
+++ lib/page/movie_page/home/components/body.dart
@@ -4,6 +4,7 @@ import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
 import 'package:fluttercrashcourse/page/movie_page/home/components/categories.dart';
 import 'package:fluttercrashcourse/page/movie_page/home/components/movie_card.dart';
+import 'dart:math' as math;
 
 class Body extends StatelessWidget {
   @override
@@ -53,9 +54,26 @@ class _MovieCarouselState extends State<MovieCarousel> {
         child: PageView.builder(
           controller: _pageController,
           itemCount: movies.length,
-          itemBuilder: (context, index) => MovieCard(movie: movies[index],)
+          itemBuilder: (context, index) => buildMovieSlider(index)
         ),
       ),
     );
   }
+
+  Widget buildMovieSlider(int index) => AnimatedBuilder(
+    animation: _pageController,
+    builder: (context, child) {
+      double value = 0;
+      if (_pageController.position.haveDimensions) {
+        value = index - _pageController.page;
+        // 180 * 0.037 = 7で、ポスターを7スクロールさせる必要があるため、0.038を使用。
+        // clampを使用するのはvalueが-1から1まで変化するから
+        value = (value * 0.038).clamp(-1, 1);
+      }
+      return Transform.rotate(
+        angle: math.pi * value,
+        child: MovieCard(movie: movies[index]),
+      );
+    },
+  );
 }
