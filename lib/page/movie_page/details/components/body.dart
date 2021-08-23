8197da3d0cccc9f2411db6b7350c26ff96289e62
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-24 06:15:20 +0900

    画面上部に画像を表示

---
+++ lib/page/movie_page/details/components/body.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercrashcourse/models/movie.dart';
+
+class Body extends StatelessWidget {
+  final Movie movie;
+
+  const Body({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 高さと横幅
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        Container(
+          // containerの縦幅をmax縦幅の4割に設定
+          height: size.height * 0.4,
+          child: Stack(
+            children: [
+              Container(
+                height: size.height * 0.4 -50,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      movie.backdrop
+                    ),
+                  )
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/page/movie_page/details/details_screen.dart
+++ lib/page/movie_page/details/details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
+import 'package:fluttercrashcourse/page/movie_page/details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Movie movie;
@@ -8,8 +9,8 @@ class DetailsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Body(movie: movie),
     );
   }
 }
