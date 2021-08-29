4cb9169a186f79abd2af26be8867d15ee94cf929
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-08-29 21:59:57 +0900

    レビュー部分作成

--- lib/page/movie_page/details/components/body.dart
+++ lib/page/movie_page/details/components/body.dart
@@ -93,11 +93,41 @@ class Body extends StatelessWidget {
                         ],
                       ),
                       // レビュー
-                      Column(),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(6),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF51CF66),
+                              borderRadius: BorderRadius.circular(2),
+                            ),
+                            child: Text(
+                              '${movie.metascoreRating}',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: kDefaultPadding / 4,),
+                          Text(
+                            'レビュー',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            '62件の重要なレビュー',
+                            style: TextStyle(color: kTextLightColor),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ), 
               ),
+              // 戻るボタン
+
             ],
           ),
         ),
