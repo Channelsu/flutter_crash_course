3c29ad549135637e9c5138d1ac0e4705607b3018
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-02 21:44:20 +0900

    映画タイトルとプラスボタン部分追加

--- lib/page/movie_page/details/components/body.dart
+++ lib/page/movie_page/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
 import 'package:fluttercrashcourse/page/movie_page/details/components/backdrop_rating.dart';
 
@@ -14,6 +15,41 @@ class Body extends StatelessWidget {
     return Column(
       children: <Widget>[
         BackdropAndRating(size: size, movie: movie),
+        // 映画タイトルとプラスボタン部分
+        Padding(
+          padding: EdgeInsets.all(kDefaultPadding),
+          child: Row(
+            children: [
+              // 映画タイトル
+              Expanded(
+                child: Column(
+                  children: [
+
+                  ],
+                )
+              ),
+              // プラスボタン
+              SizedBox(
+                height: 64,
+                width: 64,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: kSecondaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.add,
+                    size: 28,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
