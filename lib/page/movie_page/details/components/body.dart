e50e7f7ab52525bfd071032237b7b0782c3bc602
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-05 18:44:34 +0900

    映画タイトルとプラスボタンを別ファイル化

--- lib/page/movie_page/details/components/body.dart
+++ lib/page/movie_page/details/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
 import 'package:fluttercrashcourse/page/movie_page/details/components/backdrop_rating.dart';
+import 'package:fluttercrashcourse/page/movie_page/details/components/title_duration_and_fav_btn.dart';
 
 class Body extends StatelessWidget {
   final Movie movie;
@@ -16,65 +17,8 @@ class Body extends StatelessWidget {
       children: <Widget>[
         BackdropAndRating(size: size, movie: movie),
         // 映画タイトルとプラスボタン部分
-        Padding(
-          padding: EdgeInsets.all(kDefaultPadding),
-          child: Row(
-            children: [
-              // 映画タイトル
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      movie.title,
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                    SizedBox(height: kDefaultPadding / 2,),
-                    Row(
-                      children: [
-                        Text(
-                          '${movie.year}',
-                          style: TextStyle(color: kTextLightColor),
-                        ),
-                        SizedBox(width: kDefaultPadding,),
-                        Text(
-                          'PG-13',
-                          style: TextStyle(color: kTextLightColor),
-                        ),
-                        SizedBox(width: kDefaultPadding,),
-                        Text(
-                          '2時間32分',
-                          style: TextStyle(color: kTextLightColor),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ),
-              // プラスボタン
-              SizedBox(
-                height: 64,
-                width: 64,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    primary: kSecondaryColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                  ),
-                  child: Icon(
-                    Icons.add,
-                    size: 28,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        )
+        TitleDurationAndFavBtn(movie: movie)
       ],
     );
   }
 }
-
