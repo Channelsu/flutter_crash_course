36a54baa26031f536aa6d08f4c907eb75dcecf3d
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-09 06:49:44 +0900

    ユーザー登録画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fluttercrashcourse/page/sign_up_page/sign_up_page.dart';
 import './random_words.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,7 +18,8 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.purple[900],
         unselectedWidgetColor: Colors.purple[900],
       ),
-      home: RandomWords(),
+      // home: RandomWords(),
+      home: SignUpPage(),
     );
   }
 }
