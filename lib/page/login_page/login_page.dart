0666dd26c7ad6b9fecc8518d8651cbd7d4e75bf5
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-11 19:29:49 +0900

    メールアドレスとパスワードの入力欄作成

--- lib/page/login_page/login_page.dart
+++ lib/page/login_page/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,10 +8,54 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // Firebase Authenticationを利用するためのインスタンス
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // メールアドレス入力フォーム
+            Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'メールアドレス'
+                ),
+                onChanged: (String val) {
+                  email = val;
+                },
+              ),
+            ),
+            // パスワード入力フォーム
+            Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'パスワード'
+                ),
+                obscureText: true,  // パスワード非表示
+                maxLength: 20,
+                maxLengthEnforcement: MaxLengthEnforcement.none,  // 入力可能な文字数の制限を超える場合の挙動の制御
+                onChanged: (String val) {
+                  password = val;
+                },
+              ),
+            ),
+
+            // ログインボタン
+
+            // 新規アカウント作成ボタン
+
+          ],
+        ),
+      ),
     );
   }
 }
