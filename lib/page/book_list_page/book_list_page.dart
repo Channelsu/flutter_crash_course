06cca90493091868b3aa09aac9afa1989398014f
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-14 19:37:07 +0900

    本リストページの土台作成

---
+++ lib/page/book_list_page/book_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookListPage extends StatefulWidget {
+  @override
+  _BookListPageState createState() => _BookListPageState();
+}
+
+class _BookListPageState extends State<BookListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
