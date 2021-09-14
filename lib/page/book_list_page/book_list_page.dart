33ac4b29037ae08c0d408c785118f15ce7b88270
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-15 06:56:32 +0900

    firestoreのベース作成

--- lib/page/book_list_page/book_list_page.dart
+++ lib/page/book_list_page/book_list_page.dart
@@ -1,15 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class BookListPage extends StatefulWidget {
-  @override
-  _BookListPageState createState() => _BookListPageState();
-}
-
-class _BookListPageState extends State<BookListPage> {
+class BookListPage extends StatelessWidget {
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('users').snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _usersStream,
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+    
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+    
+            return ListView(
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['full_name']),
+                  subtitle: Text(data['company']),
+                );
+              }).toList(),
+            );
+          },
+        ),
+      ),
     );
   }
 }
