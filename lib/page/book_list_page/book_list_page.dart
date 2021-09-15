864d9d406c041254a7279cbe65e60a52a3b45865
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-16 06:20:15 +0900

    カラム名修正

--- lib/page/book_list_page/book_list_page.dart
+++ lib/page/book_list_page/book_list_page.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BookListPage extends StatelessWidget {
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('users').snapshots();
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('books').snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +22,8 @@ class BookListPage extends StatelessWidget {
               children: snapshot.data.docs.map((DocumentSnapshot document) {
               Map<String, dynamic> data = document.data() as Map<String, dynamic>;
                 return ListTile(
-                  title: Text(data['full_name']),
-                  subtitle: Text(data['company']),
+                  title: Text(data['title']),
+                  subtitle: Text(data['author']),
                 );
               }).toList(),
             );
