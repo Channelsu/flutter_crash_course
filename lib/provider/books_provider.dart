28fd69478949872144572b1549080a5c0a636fff
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-19 11:25:55 +0900

    book_provider作成

--- lib/provider/books_provider.dart
+++ lib/provider/books_provider.dart
@@ -1,8 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttercrashcourse/models/book.dart';
 
 class BooksProvider extends ChangeNotifier {
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('books').snapshots();
+  
+  // リストがnullになる可能性もあるよという意味
+  // List<Book>? books;
+  List<Book> books = [];
 
   void fetchBookList() {
-
+    _usersStream.listen((QuerySnapshot snapshot) {
+      print(snapshot);
+      final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
+        // 以下Bookを作る
+        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+        final String title = data['title'];
+        final String author = data['author'];
+        return Book(title, author);
+      }).toList();
+      this.books = books;
+      notifyListeners();
+    });
   }
 }
