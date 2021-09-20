eb766cafa1553660a8e54cb6b51f9f20bebd6ccd
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-21 08:25:34 +0900

    初回のみ発火のメソッド追加

--- lib/provider/books_provider.dart
+++ lib/provider/books_provider.dart
@@ -4,7 +4,7 @@ import 'package:fluttercrashcourse/models/book.dart';
 
 class BooksProvider extends ChangeNotifier {
   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('books').snapshots();
-  
+  final _bookCollection = FirebaseFirestore.instance.collection('books');
   // リストがnullになる可能性もあるよという意味
   // List<Book>? books;
   List<Book> books = [];
@@ -14,7 +14,7 @@ class BooksProvider extends ChangeNotifier {
       // DocumentSnapshot型からBook型へ変換処理
       final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
         // 以下Bookを作る
-        print(document.data());
+        // print(document.data());
         Map<String, dynamic> data = document.data() as Map<String, dynamic>;
         final String title = data['title'];
         final String author = data['author'];
@@ -23,5 +23,20 @@ class BooksProvider extends ChangeNotifier {
       this.books = books;
       notifyListeners();
     });
+  }
+
+  void fecthBookList2() async {
+    // .get()は変化があったら発火するのではなく、１回しか取らない。⇨リアルタイムで変化があっても反映されない。
+    final QuerySnapshot snapshot = await _bookCollection.get();
+
+    final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String title = data['title'];
+      final String author = data['author'];
+      return Book(title, author);
+    }).toList();
+
+    this.books = books;
+    notifyListeners();
   }
 }
