08d18123ef33be5c0287ac7ee7d11081e2583781
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-09-22 21:59:05 +0900

    タグ部分作成

--- lib/page/movie_page/details/components/body.dart
+++ lib/page/movie_page/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttercrashcourse/components/genre_card.dart';
 import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
 import 'package:fluttercrashcourse/page/movie_page/details/components/backdrop_rating.dart';
@@ -20,7 +21,19 @@ class Body extends StatelessWidget {
         // 映画タイトルとプラスボタン部分
         TitleDurationAndFavBtn(movie: movie),
         // タグ部分
-        
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+          child: SizedBox(
+            height: 36,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: movie.genra.length, 
+              itemBuilder: (BuildContext context, int index) => GenreCard(
+                genre: movie.genra[index],
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
