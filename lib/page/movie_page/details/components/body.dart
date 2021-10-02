5c9a765991719cf16fd2f293d1af9054ba466f2c
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-10-02 17:31:15 +0900

    出演者の顔画像表示

--- lib/page/movie_page/details/components/body.dart
+++ lib/page/movie_page/details/components/body.dart
@@ -80,3 +80,32 @@ class CastAndCrew extends StatelessWidget {
     );
   }
 }
+
+class CastCard extends StatelessWidget {
+  final Map cast;
+
+  const CastCard({Key key, this.cast}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: kDefaultPadding),
+      width: 80,
+      child: Column(
+        children: [
+          Container(
+            height: 80,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  cast['image'],
+                ),
+              )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
