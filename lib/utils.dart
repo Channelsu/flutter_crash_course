e1921e8298db3436236b4d1a53997b1e2477ea3a
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-10-11 06:07:45 +0900

    buildTextsとgoBackメソッド作成

--- lib/utils.dart
+++ lib/utils.dart
@@ -2,6 +2,40 @@ import 'package:flutter/material.dart';
 
 class Utils {
   
+  // 前のページに戻るメソッド
+  static void goBack(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  static Widget buildTexts(List<String> texts) {
+    List<Widget> textWidgetList = [];
+
+    for(var i = 0; i < texts.length; i++) {
+      textWidgetList.add(Text(texts[i]));
+    }
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: textWidgetList
+    );
+  }
+
+  // Widget _buildSpotName() {
+  //   return TextFormField(
+  //     decoration: InputDecoration(labelText: '駐輪所名',),
+  //     maxLength: 20,
+  //     validator: (String value) {
+  //       if(value.isEmpty) {
+  //         return '必須入力です';
+  //       }
+  //       return null;
+  //     },
+  //     onSaved: (String value) {
+  //       _spotName = value;
+  //     },
+  //   );
+  // }
+
   // 現在日時を取得し、文字列にして返すメソッド
   static String getNow() {
     // 2021-10-09 08:47:40.397734
