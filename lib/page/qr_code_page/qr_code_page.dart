0491c3c708e740fa6fec604c0c3bd1a8f1683037
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-10-15 22:51:21 +0900

    qrコード画面の土台作成

---
+++ lib/page/qr_code_page/qr_code_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QRCodePage extends StatefulWidget {
+
+  @override
+  _QRCodePageState createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
