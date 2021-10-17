b2b98986872cbbc068e32b95acf5987d5dc10b58
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-10-17 16:41:24 +0900

    qrコード途中まで作成

--- lib/page/qr_code_page/qr_code_page.dart
+++ lib/page/qr_code_page/qr_code_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QRCodePage extends StatefulWidget {
 
@@ -7,10 +8,24 @@ class QRCodePage extends StatefulWidget {
 }
 
 class _QRCodePageState extends State<QRCodePage> {
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController controller;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SafeArea(
+      child: Scaffold(
+        body: buildQrView(context),
+      ),
     );
+  }
+
+  Widget buildQrView(BuildContext context) => QRView(
+    key: qrKey,
+    onQRViewCreated: onQRViewCreated,
+  );
+
+  void onQRViewCreated(QRViewController controller) {
+
   }
 }
