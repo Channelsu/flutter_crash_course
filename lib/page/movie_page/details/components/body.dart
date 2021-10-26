40b890a66eb44d8be61c23dcf8dbfdecd6310b2a
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-10-27 07:25:06 +0900

    不要なインポート文削除

--- lib/page/movie_page/details/components/body.dart
+++ lib/page/movie_page/details/components/body.dart
@@ -3,7 +3,6 @@ import 'package:fluttercrashcourse/constants.dart';
 import 'package:fluttercrashcourse/models/movie.dart';
 import 'package:fluttercrashcourse/page/movie_page/details/components/backdrop_rating.dart';
 import 'package:fluttercrashcourse/page/movie_page/details/components/cast_and_crew.dart';
-import 'package:fluttercrashcourse/page/movie_page/details/components/cast_card.dart';
 import 'package:fluttercrashcourse/page/movie_page/details/components/genres.dart';
 import 'package:fluttercrashcourse/page/movie_page/details/components/title_duration_and_fav_btn.dart';
 
